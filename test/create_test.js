0297391c769172d8c617bd4db2f3a16b12ed8ca3
Taka Okunishi <okunishitaka.com@gmail.com>
2016-05-27 22:13:06 +0900

    Initial commit

---
+++ test/create_test.js
@@ -0,0 +1,25 @@
+/**
+ * Test case for create.
+ * Runs with mocha.
+ */
+'use strict'
+
+const create = require('../lib/create.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('create', () => {
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Create', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
---
+++ test/sugo_terminal_test.js
@@ -0,0 +1,25 @@
+/**
+ * Test case for sugoTerminal.
+ * Runs with mocha.
+ */
+'use strict'
+
+const sugoTerminal = require('../lib/sugo_terminal.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('sugo-terminal', () => {
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Sugo terminal', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
