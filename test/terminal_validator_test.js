71a9293a16293e7500142383becfdec81a7b7afa
Taka Okunishi <okunishitaka.com@gmail.com>
2016-06-14 17:43:11 +0900

    Version incremented to 1.0.6

---
+++ test/terminal_validator_test.js
@@ -0,0 +1,25 @@
+/**
+ * Test case for terminalValidator.
+ * Runs with mocha.
+ */
+'use strict'
+
+const terminalValidator = require('../lib/validating/terminal_validator.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('terminal-validator', () => {
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Terminal validator', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
