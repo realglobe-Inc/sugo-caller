6d6e504d6032deda5b35f770e7186f612cc5d7fa
Taka Okunishi <okunishitaka.com@gmail.com>
2016-08-30 13:17:12 +0900

    Split actor access bundle

---
+++ test/actor_access_bundle_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for actorAccessBundle.
+ * Runs with mocha.
+ */
+'use strict'
+
+const actorAccessBundle = require('../lib/accessing/actor_access_bundle.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('actor-access-bundle', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Actor access bundle', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
