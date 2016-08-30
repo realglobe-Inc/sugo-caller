ded42b7f3e77a58dd2b631e5c82fb712f3fa5090
Taka Okunishi <okunishitaka.com@gmail.com>
2016-08-30 13:23:38 +0900

    Refactoring

---
+++ test/actor_access_module_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for actorAccessModule.
+ * Runs with mocha.
+ */
+'use strict'
+
+const actorAccessModule = require('../lib/accessing/actor_access_module.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('actor-access-module', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Actor access module', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
